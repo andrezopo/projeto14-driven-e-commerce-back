03722ed2065a4c87b97e5de12866c35f42658fa9
André Zopolato <andrezopo@gmail.com>
2022-07-07 20:54:36 -0300

    testa conexão com o banco de dados

--- src/controllers/helloController.js
+++ src/controllers/helloController.js
@@ -1,7 +1,7 @@
 import { db } from "../dbStrategy/mongo.js";
 
 export async function hello(req, res) {
-  await db.collection("teste").insertOne({ teste: teste });
+  await db.collection("teste").insertOne({ teste: "teste" });
   res.status(200).send("Hello world");
 
   console.log("Hello");
