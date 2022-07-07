278d5d9ed4d841a71d51445c912a570c9266251e
André Zopolato <andrezopo@gmail.com>
2022-07-07 20:56:02 -0300

    finaliza etapa de deploy

--- src/controllers/helloController.js
+++ src/controllers/helloController.js
@@ -1,7 +1,6 @@
 import { db } from "../dbStrategy/mongo.js";
 
 export async function hello(req, res) {
-  await db.collection("teste").insertOne({ teste: "teste" });
   res.status(200).send("Hello world");
 
   console.log("Hello");
