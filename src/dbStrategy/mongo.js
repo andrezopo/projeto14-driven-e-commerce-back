b7561846e2978c487394a23fa354f24f23495ac3
André Zopolato <andrezopo@gmail.com>
2022-07-07 20:45:09 -0300

    testa conexão com o banco de dados

--- src/dbStrategy/mongo.js
+++ src/dbStrategy/mongo.js
@@ -8,7 +8,7 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
-  db = mongoClient.db("Cluster0");
+  db = mongoClient.db("organistore");
 });
 
 export default { db, ObjectId };
