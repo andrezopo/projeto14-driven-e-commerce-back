5532054a3aae2fafe6b2598b6be48cf55aa28232
jaquecaye <jaquelinecaye@gmail.com>
2022-07-10 10:28:48 -0300

    Implementacao funcao para ordenar por preco e alfabetico

---
+++ src/routes/productRouter.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { createProduct,  showProducts} from "../controllers/productsController.js"
+import validateBody from "../middlewares/validateBodySchema.js";
+import productSchema from "../schemas/productSchema.js";
+
+const router = Router();
+
+router.post("/produtos", validateBody(productSchema), createProduct);
+
+router.get("/produtos", showProducts);
+
+export default router;
