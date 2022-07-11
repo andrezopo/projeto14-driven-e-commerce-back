f6fdecc91b172a7aa9c055ec80a79492359ef339
jaquecaye <jaquelinecaye@gmail.com>
2022-07-10 21:37:26 -0300

    Lógica para renderizar produtos no carrinho e deletar produtos

--- src/controllers/userFeaturesController.js
+++ src/controllers/userFeaturesController.js
@@ -13,21 +13,62 @@ export async function addToCard(request, response) {
     categoria: produto.categoria,
   };
 
-  console.log(produtoCarrinho)
+  console.log(produtoCarrinho);
 
   try {
-    const produtoRepetido = await db.collection("carrinho").find({descricao: produto.descricao}).toArray()
+    const produtoRepetido = await db
+      .collection("carrinho")
+      .find({ descricao: produto.descricao })
+      .toArray();
 
-    console.log(produtoRepetido)
+    console.log(produtoRepetido);
 
-    if (produtoRepetido.length !== 0){
-        response.status(400).send("Produto já adicionado ao carrinho. Tente inserir um produto diferente.")
-        return
+    if (produtoRepetido.length !== 0) {
+      response
+        .status(400)
+        .send(
+          "Produto já adicionado ao carrinho. Tente inserir um produto diferente."
+        );
+      return;
     }
 
     await db.collection("carrinho").insertOne(produtoCarrinho);
     response.status(201).send(produtoCarrinho);
+  } catch (error) {
+    response.status(500).send();
+  }
+}
 
+export async function showProducts(request, response) {
+  const usuario = response.locals.usuario;
+
+  try {
+    const produtosCarrinho = await db
+      .collection("carrinho")
+      .find({ email: usuario.email })
+      .toArray();
+    response.status(201).send(produtosCarrinho);
+  } catch (error) {
+    response.status(500).send();
+  }
+}
+
+export async function deleteProduct(request, response) {
+  const { authorization, idproduto } = request.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  const usuario = await db.collection("sessoes").findOne({ token });
+
+  if (!usuario) {
+    response.status(422).send();
+    return;
+  }
+
+  try {
+    const produtoDeletado = await db
+      .collection("carrinho")
+      .deleteOne({ _id: ObjectId(idproduto) });
+    response.status(201).send(produtoDeletado);
   } catch (error) {
     response.status(500).send();
   }
--- src/routes/userFeaturesRouter.js
+++ src/routes/userFeaturesRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { addToCard} from "../controllers/userFeaturesController.js"
+import { addToCard, showProducts, deleteProduct} from "../controllers/userFeaturesController.js"
 import validateUser from '../middlewares/validateUser.js';
 import validateBody from "../middlewares/validateBodySchema.js";
 import productSchema from "../schemas/productSchema.js";
@@ -8,5 +8,9 @@ import productSchema from "../schemas/productSchema.js";
 const router = Router();
 
 router.post("/carrinho", validateUser, validateBody(productSchema), addToCard);
+
+router.get("/carrinho", validateUser, showProducts);
+
+router.delete("/carrinho", deleteProduct)
 
 export default router;
