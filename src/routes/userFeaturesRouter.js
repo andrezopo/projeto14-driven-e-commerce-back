55dc797e0bac5d46d1b407b444170dd1c55949f8
jaquecaye <jaquelinecaye@gmail.com>
2022-07-11 15:50:40 -0300

    Tela Carrinho implementação de valores dos produtos

--- src/routes/userFeaturesRouter.js
+++ src/routes/userFeaturesRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { addToCard, showProducts, deleteProduct, changePassword, confirmPurchase} from "../controllers/userFeaturesController.js"
+import { addToCard, showProducts, deleteProduct, updateProduct, changePassword, confirmPurchase} from "../controllers/userFeaturesController.js"
 import validateUser from '../middlewares/validateUser.js';
 import validateBody from "../middlewares/validateBodySchema.js";
 import productSchema from "../schemas/productSchema.js";
@@ -14,6 +14,8 @@ router.post("/carrinho", validateUser, validateBody(productSchema), addToCard);
 router.get("/carrinho", validateUser, showProducts);
 
 router.delete("/carrinho", deleteProduct)
+
+router.put("/carrinho", updateProduct)
 
 router.put(
   "/senha",
